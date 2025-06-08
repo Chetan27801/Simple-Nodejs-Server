af257b8bd70825c492c52f398b1767e06d5fc83a
Chetan <c27801j@gmail.com>
2025-06-08 20:59:21 +0530

    first commit for server

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.get("/health", (req, res) => {
+	res.send("Everything is working fine");
+});
+
+app.get("/products", (req, res) => {
+	res.send("This is Product page");
+});
+
+app.listen(8080, () => {
+	console.log("Server is running on port 8080");
+});
